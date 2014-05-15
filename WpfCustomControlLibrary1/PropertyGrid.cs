c3dacd75bd9e6a344507fd18e4181f7e05a84f47
Martijn-vd-H <martijn_vd_hoven@hotmail.com>
2014-05-15 13:51:48 +0200

    Added code

---
+++ WpfCustomControlLibrary1/PropertyGrid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PropertyGrid
+{
+    public class PropertyGrid : ItemsControl
+    {
+        public Brush GridColor
+        {
+            get { return (Brush)GetValue(GridColorProperty); }
+            set { SetValue(GridColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty GridColorProperty =
+            DependencyProperty.Register("GridColor", typeof(Brush), typeof(PropertyGrid), new UIPropertyMetadata(new SolidColorBrush(Colors.Transparent)));
+
+        static PropertyGrid()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PropertyGrid), new FrameworkPropertyMetadata(typeof(PropertyGrid)));
+        }
+    }
+}
---
+++ WpfCustomControlLibrary1/PropertyGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyGrid
+{
+    public class PropertyGroup
+    {
+        public string Name { get; set; }
+        public List<PropertyGridItem> Items { get; set; }
+
+        public PropertyGroup()
+        {
+            Items = new List<PropertyGridItem>();
+            Name = "";
+        }
+    }
+}
