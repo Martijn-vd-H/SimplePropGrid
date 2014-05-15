8ab015e77ddd415bc9e70531290cc018153b68f2
Martijn-vd-H <martijn_vd_hoven@hotmail.com>
2014-05-15 12:23:27 +0200

    Added code

---
+++ PropGridTest/PropertyGrid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace PropGridTest
+{
+    public class PropertyGrid : ItemsControl
+    {
+        public Brush GridColor
+        {
+            get { return (Brush)GetValue(GridColorProperty); }
+            set { SetValue(GridColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty GridColorProperty =
+            DependencyProperty.Register("GridColor", typeof(Brush), typeof(PropertyGrid), new UIPropertyMetadata(new SolidColorBrush(Colors.Transparent)));
+
+        static PropertyGrid()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PropertyGrid), new FrameworkPropertyMetadata(typeof(PropertyGrid)));
+        }
+    }
+}
---
+++ PropGridTest/ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropGridTest
+{
+    class ViewModel
+    {
+        public List<PropertyGroup> Groups { get; set; }
+
+        public ViewModel()
+        {
+            Groups = new List<PropertyGroup>();
+
+            PropertyGroup group1 = new PropertyGroup();
+            group1.Name = "Group1";
+            group1.Items.Add(new PropertyGridItem("Item1", "test"));
+            group1.Items.Add(new PropertyGridItem("Item2", 300));
+            group1.Items.Add(new PropertyGridItem("Item3", true));
+            group1.Items.Add(new PropertyGridItem("Item4", 5.2));
+            Groups.Add(group1);
+
+            PropertyGroup group2 = new PropertyGroup();
+            group2.Name = "Group2";
+            group2.Items.Add(new PropertyGridItem("Item1", "test"));
+            group2.Items.Add(new PropertyGridItem("Item2", 300));
+            group2.Items.Add(new PropertyGridItem("Item3", true));
+            group2.Items.Add(new PropertyGridItem("Item4", 5.2));
+            Groups.Add(group2);
+        }
+    }
+}
